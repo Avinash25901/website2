22ad5030d118e3377674c576b880c69d77b9ebca
Avinash <115500130+Avinash25901@users.noreply.github.com>
2023-12-05 03:10:17 +0530

    Add files via upload

---
+++ space-script.js
@@ -0,0 +1,10 @@
+// You can add JavaScript functionality here if needed
+document.getElementById('play-video').addEventListener('click', function() {
+    // Add code to play video trailer
+    alert('Video trailer playing...');
+});
+
+document.getElementById('learn-more').addEventListener('click', function() {
+    // Add code to navigate to the detailed mission page
+    alert('Learn more about our mission...');
+});
